df961694250f8129d90b5cb6538accaea6cf997a
Kanishk Vishwakarma <kanishk.vishwa2001@gmail.com>
2022-08-29 19:40:30 +0530

    Objects instead of tables, single channel

--- dataset_generation/scripts/seg_mask.py
+++ dataset_generation/scripts/seg_mask.py
@@ -127,7 +127,7 @@ class dataset:
 
                 info_target = self.sentences_target + 'm' + str(map_num) + "_s" + str(sentence_index) + ".txt"
                 info_target = open(info_target, 'a')
-                sentence = str(sentence.split('_')[0]) + ' ' + str(sentence.split('_')[1])
+                print(sentence)
                 info_target.write(str(sentence))
                 info_target.close()
 
--- dataset_generation/scripts/sentence_generator.py
+++ dataset_generation/scripts/sentence_generator.py
@@ -21,6 +21,7 @@ class sentences:
         self.Sentences = {}
         self.Places = []
         self.nDoors = self.nRooms = self.nTables = 0
+        self.object_list = ['Table', 'Sofa', 'Almirah', 'Lamp', 'Carpet', 'Fan', 'Computer', 'Tea', 'Plant', 'Telephone']
         for loc in locations:
             if (loc[0][0:-2] == 'Room'):
                 self.nRooms +=1
@@ -35,21 +36,21 @@ class sentences:
         #for room in range(0, self.nRooms):
         #    self.Places.append('Room_' + str(room+1))
         for table in range(0, self.nTables):
-            self.Places.append('Table_' + str(table+1))
+            self.Places.append(self.object_list[table+1])
 
-        for imperative in Imperatives:
-            for connective in Connectives:
-                for place1 in range(0, len(self.Places)-1):
-                    remaining_places = self.Places[:place1] + self.Places[(place1+1):]
-                    for place2 in range(0, len(remaining_places)-1):
-                        sentence = imperative + self.Places[place1] + connective + imperative + remaining_places[place2]
-                        waypoints = (self.Places[place1], remaining_places[place2])
-                        self.Sentences[sentence] = waypoints
         #for imperative in Imperatives:
-        #   for place in self.Places:
-        #       sentence = imperative + place
-        #       waypoints = ((place),)
-        #       self.Sentences[sentence] = waypoints
+        #    for connective in Connectives:
+        #        for place1 in range(0, len(self.Places)-1):
+        #            remaining_places = self.Places[:place1] + self.Places[(place1+1):]
+        #            for place2 in range(0, len(remaining_places)-1):
+        #                sentence = imperative + self.Places[place1] + connective + imperative + remaining_places[place2]
+        #                waypoints = (self.Places[place1], remaining_places[place2])
+        #                self.Sentences[sentence] = waypoints
+        for imperative in Imperatives:
+           for place in self.Places:
+               sentence = imperative + place
+               waypoints = ((place),)
+               self.Sentences[sentence] = waypoints
     def returnSentences(self):
         return self.Sentences
 
