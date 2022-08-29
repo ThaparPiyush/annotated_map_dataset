8ea306a685cd065da658b53542177fe7ef354ac6
Kanishk Vishwakarma <kanishk.vishwa2001@gmail.com>
2022-08-29 14:36:07 +0530

    Double channel dataset

--- dataset_generation/scripts/splitter.py
+++ dataset_generation/scripts/splitter.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import shutil
+import pdb
 
 data_path = '/scratch/wheelchair/annotated_map_dataset/dataset_generation/data'
 
@@ -9,6 +10,12 @@ split_ratio = [0.75, 0.25]   # train, val, test
 data_folders = ['color_map_image', 'map_seg_mask', 'waypoints']
 prev_count = 0
 count = 0 
+def return_int(string):
+    emp_str = ""
+    for m in string:
+        if m.isdigit():
+            emp_str = emp_str + m
+    return emp_str
 for i in range(len(directories)):
     path = ''
     count = count + split_ratio[i] * len(os.listdir(data_path+'/color_map_image'))
@@ -21,10 +28,12 @@ for i in range(len(directories)):
         if not os.path.exists(path):
             os.mkdir(path)
 
+        pdb.set_trace()
         # create the color_map etc directory
         target_path = os.path.join(data_path, data_folders[j])
         targetf = [name for name in os.listdir(target_path) if os.path.isfile(os.path.join(target_path, name))]
         # color_len = len(targetf)
+        targetf = sorted(targetf)
 
         path = os.path.join(path, data_folders[j])
         if not os.path.exists(path):
@@ -34,7 +43,7 @@ for i in range(len(directories)):
         # copy files
         for f in targetf:
             print(f.split('_'))
-            if int(f.split('_')[1]) <= count and int(f.split('_')[1]) > prev_count:
+            if int(return_int(f.split('_')[1])) <= count and int(return_int(f.split('_')[1])) > prev_count:
                 shutil.copy2(os.path.join(target_path, f), path)
 
     print("Directory '% s' created" % directories[i])
